95c06c0a34bb0c9ec209f4b375cfb7ad938d4068
Umesh <umeshkumar131993@gmail.com>
2021-01-10 17:09:13 +0530

    form control.

--- src/app/shared-module/create-roaster/create-roaster.component.ts
+++ src/app/shared-module/create-roaster/create-roaster.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, FormArray } from '@angular/forms';
-declare var $:any
+declare var $: any
 
 @Component({
   selector: 'app-create-roaster',
@@ -9,13 +9,14 @@ declare var $:any
 })
 export class CreateRoasterComponent implements OnInit {
 
-  firstForm:FormGroup;
-  secondForm:FormArray;
-  roasterList:any = [];
-  constructor(private fb:FormBuilder) {
+  firstForm: FormGroup;
+  secondForm: FormArray;
+  roasterList: any = [];
+  constructor(private fb: FormBuilder) {
     this.firstForm = this.fb.group({
-      user:[''],
-      user_type:['']
+      user: [''],
+      user_type: [''],
+      empArr: this.fb.array([this.createRow()])
     });
     // this.secondForm = this.fb.group({
     //   list:new FormArray([
@@ -32,257 +33,264 @@ export class CreateRoasterComponent implements OnInit {
   ngOnInit() {
     this.callingJQuery();
   }
-
-  public onSelect(){
-    console.log("fir-===========>>>",this.firstForm.value)
+  createRow() {
+    return this.fb.group({
+      id: [''],
+      name: [''],
+      pickup: []
+    })
   }
 
-  public callingJQuery(){
+  public onSelect() {
+    console.log("fir-===========>>>", this.firstForm.value)
+  }
+
+  public callingJQuery() {
     $('.mdb-select').materialSelect();
     $('.timepicker').pickatime({
       twelvehour: true
     });
   }
 
-  public list(){
+  public list() {
     this.roasterList = [
       {
-        id:1,
-        name:'AAA',
-        pickup:[
+        id: 1,
+        name: 'AAA',
+        pickup: [
           {
-            id:1,
-            pickup_time:'',
-            drop_time:'',
-            location:''
+            id: 1,
+            pickup_time: '',
+            drop_time: '',
+            location: ''
           },
           {
-            id:2,
-            pickup_time:'',
-            drop_time:'',
-            location:''
+            id: 2,
+            pickup_time: '',
+            drop_time: '',
+            location: ''
           },
           {
-            id:3,
-            pickup_time:'',
-            drop_time:'',
-            location:''
+            id: 3,
+            pickup_time: '',
+            drop_time: '',
+            location: ''
           },
           {
-            id:4,
-            pickup_time:'',
-            drop_time:'',
-            location:''
+            id: 4,
+            pickup_time: '',
+            drop_time: '',
+            location: ''
           },
           {
-            id:5,
-            pickup_time:'',
-            drop_time:'',
-            location:''
+            id: 5,
+            pickup_time: '',
+            drop_time: '',
+            location: ''
           },
           {
-            id:6,
-            pickup_time:'',
-            drop_time:'',
-            location:''
+            id: 6,
+            pickup_time: '',
+            drop_time: '',
+            location: ''
           },
           {
-            id:7,
-            pickup_time:'',
-            drop_time:'',
-            location:''
+            id: 7,
+            pickup_time: '',
+            drop_time: '',
+            location: ''
           }
         ]
       },
       {
-        id:2,
-        name:'BBB',
-        pickup:[
+        id: 2,
+        name: 'BBB',
+        pickup: [
           {
-            id:1,
-            pickup_time:'',
-            drop_time:'',
-            location:''
+            id: 1,
+            pickup_time: '',
+            drop_time: '',
+            location: ''
           },
           {
-            id:2,
-            pickup_time:'',
-            drop_time:'',
-            location:''
+            id: 2,
+            pickup_time: '',
+            drop_time: '',
+            location: ''
           },
           {
-            id:3,
-            pickup_time:'',
-            drop_time:'',
-            location:''
+            id: 3,
+            pickup_time: '',
+            drop_time: '',
+            location: ''
           },
           {
-            id:4,
-            pickup_time:'',
-            drop_time:'',
-            location:''
+            id: 4,
+            pickup_time: '',
+            drop_time: '',
+            location: ''
           },
           {
-            id:5,
-            pickup_time:'',
-            drop_time:'',
-            location:''
+            id: 5,
+            pickup_time: '',
+            drop_time: '',
+            location: ''
           },
           {
-            id:6,
-            pickup_time:'',
-            drop_time:'',
-            location:''
+            id: 6,
+            pickup_time: '',
+            drop_time: '',
+            location: ''
           },
           {
-            id:7,
-            pickup_time:'',
-            drop_time:'',
-            location:''
+            id: 7,
+            pickup_time: '',
+            drop_time: '',
+            location: ''
           }
         ]
       },
       {
-        id:2,
-        name:'CCC',
-        pickup:[
+        id: 2,
+        name: 'CCC',
+        pickup: [
           {
-            id:1,
-            pickup_time:'',
-            drop_time:'',
-            location:''
+            id: 1,
+            pickup_time: '',
+            drop_time: '',
+            location: ''
           },
           {
-            id:2,
-            pickup_time:'',
-            drop_time:'',
-            location:''
+            id: 2,
+            pickup_time: '',
+            drop_time: '',
+            location: ''
           },
           {
-            id:3,
-            pickup_time:'',
-            drop_time:'',
-            location:''
+            id: 3,
+            pickup_time: '',
+            drop_time: '',
+            location: ''
           },
           {
-            id:4,
-            pickup_time:'',
-            drop_time:'',
-            location:''
+            id: 4,
+            pickup_time: '',
+            drop_time: '',
+            location: ''
           },
           {
-            id:5,
-            pickup_time:'',
-            drop_time:'',
-            location:''
+            id: 5,
+            pickup_time: '',
+            drop_time: '',
+            location: ''
           },
           {
-            id:6,
-            pickup_time:'',
-            drop_time:'',
-            location:''
+            id: 6,
+            pickup_time: '',
+            drop_time: '',
+            location: ''
           },
           {
-            id:7,
-            pickup_time:'',
-            drop_time:'',
-            location:''
+            id: 7,
+            pickup_time: '',
+            drop_time: '',
+            location: ''
           }
         ]
       },
       {
-        id:2,
-        name:'DDD',
-        pickup:[
+        id: 2,
+        name: 'DDD',
+        pickup: [
           {
-            id:1,
-            pickup_time:'',
-            drop_time:'',
-            location:''
+            id: 1,
+            pickup_time: '',
+            drop_time: '',
+            location: ''
           },
           {
-            id:2,
-            pickup_time:'',
-            drop_time:'',
-            location:''
+            id: 2,
+            pickup_time: '',
+            drop_time: '',
+            location: ''
           },
           {
-            id:3,
-            pickup_time:'',
-            drop_time:'',
-            location:''
+            id: 3,
+            pickup_time: '',
+            drop_time: '',
+            location: ''
           },
           {
-            id:4,
-            pickup_time:'',
-            drop_time:'',
-            location:''
+            id: 4,
+            pickup_time: '',
+            drop_time: '',
+            location: ''
           },
           {
-            id:5,
-            pickup_time:'',
-            drop_time:'',
-            location:''
+            id: 5,
+            pickup_time: '',
+            drop_time: '',
+            location: ''
           },
           {
-            id:6,
-            pickup_time:'',
-            drop_time:'',
-            location:''
+            id: 6,
+            pickup_time: '',
+            drop_time: '',
+            location: ''
           },
           {
-            id:7,
-            pickup_time:'',
-            drop_time:'',
-            location:''
+            id: 7,
+            pickup_time: '',
+            drop_time: '',
+            location: ''
           }
         ]
       },
       {
-        id:2,
-        name:'EEE',
-        pickup:[
+        id: 2,
+        name: 'EEE',
+        pickup: [
           {
-            id:1,
-            pickup_time:'',
-            drop_time:'',
-            location:''
+            id: 1,
+            pickup_time: '',
+            drop_time: '',
+            location: ''
           },
           {
-            id:2,
-            pickup_time:'',
-            drop_time:'',
-            location:''
+            id: 2,
+            pickup_time: '',
+            drop_time: '',
+            location: ''
           },
           {
-            id:3,
-            pickup_time:'',
-            drop_time:'',
-            location:''
+            id: 3,
+            pickup_time: '',
+            drop_time: '',
+            location: ''
           },
           {
-            id:4,
-            pickup_time:'',
-            drop_time:'',
-            location:''
+            id: 4,
+            pickup_time: '',
+            drop_time: '',
+            location: ''
           },
           {
-            id:5,
-            pickup_time:'',
-            drop_time:'',
-            location:''
+            id: 5,
+            pickup_time: '',
+            drop_time: '',
+            location: ''
           },
           {
-            id:6,
-            pickup_time:'',
-            drop_time:'',
-            location:''
+            id: 6,
+            pickup_time: '',
+            drop_time: '',
+            location: ''
           },
           {
-            id:7,
-            pickup_time:'',
-            drop_time:'',
-            location:''
+            id: 7,
+            pickup_time: '',
+            drop_time: '',
+            location: ''
           }
         ]
       }
